8c2ac0f818642ad3b871383d82e3190629ab3939
Jarno Wieman <Jarno.Wieman@infosupport.com>
2025-11-28 16:16:17 +0100

    ADD: more aliases

--- setup_git.sh
+++ setup_git.sh
@@ -6,8 +6,12 @@ git config --global push.autoSetupRemote true
 # Aliases
 git config --global alias.st "status"
 git config --global alias.sw "switch"
+git config --global alias.br "branch"
+git config --global alias.c "commit"
 git config --global alias.cm "commit -m"
 git config --global alias.cma "commit -a -m"
+git config --global alias.cmaa "commit -a --amend -m"
+git config --global alias.ca "commit --amend"
 git config --global alias.pl "pull"
 git config --global alias.plr "pull --rebase"
 git config --global alias.ps "push"
